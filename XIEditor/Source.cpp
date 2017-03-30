a51a3273ceb1752703eb84f89086fcc5c7c8dca2
Ethan <ethanbinya@aol.com>
2017-03-30 12:49:05 -0400

    removed startMsg

--- XIEditor/Source.cpp
+++ XIEditor/Source.cpp
@@ -6,13 +6,6 @@
 void main(int argc, char *argv[]) {
 	XIEditor editor(argv[1]);
 
-	std::string startMsg = std::string("\t\t\t\t-- XI-Editor --\n") +
-		";q to quit, " +
-		"k to move up, " +
-		"j to move down, " +
-		"l to move right, " +
-		"h to move left.\n\n\n";
-
 	system("cls");
 
 	editor.start();
