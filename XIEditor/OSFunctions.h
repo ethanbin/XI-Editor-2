89042356e6a597651b798200b01d864a822456fa
Ethan <ethanbinya@aol.com>
2017-05-01 16:01:06 -0400

    removed force option and instead changed functions to inline

--- XIEditor/OSFunctions.h
+++ XIEditor/OSFunctions.h
@@ -6,20 +6,20 @@
 //Windows 32-bit and 64-bit
 #ifdef _WIN32
 	#include <Windows.h>
-	void clrscrn() {
+	inline void clrscrn() {
 		system("cls");
 	}
 
-	void consoleBlackOnWhite() {
+	inline void consoleBlackOnWhite() {
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xF0);
 	}
 
-	void consoleWhiteOnBlack() {
+	inline void consoleWhiteOnBlack() {
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x07);
 	}
 
 	//takes int x and y for coordinates to move cursor to.
-	void moveCursorTo(int x, int y) {
+	inline void moveCursorTo(int x, int y) {
 		COORD coord;
 		coord.X = x;
 		coord.Y = y;
@@ -31,38 +31,38 @@
 #include "TargetConditionals.h"
 	//mac OS
 	#if TARGET_OS_MAC
-	void clrscrn() {}
+	inline void clrscrn() {}
 
-	void consoleBlackOnWhite() {}
+	inline void consoleBlackOnWhite() {}
 
-	void consoleWhiteOnBlack() {}
+	inline void consoleWhiteOnBlack() {}
 
 	//takes int x and y for coordinates to move cursor to.
-		void cursor(int x, int y) {}
+	inline void cursor(int x, int y) {}
 	#else
 		#error "Unknown Apple platform"
 	#endif
 
 //linux
 #elif __linux__
-	void clrscrn() {}
+	inline void clrscrn() {}
 
-	void consoleBlackOnWhite() {}
+	inline void consoleBlackOnWhite() {}
 
-	void consoleWhiteOnBlack() {}
+	inline void consoleWhiteOnBlack() {}
 	
 	//takes int x and y for coordinates to move cursor to.
-	void cursor(int x, int y) {}
+	inline void cursor(int x, int y) {}
 //unix
 #elif __unix__
-	void clrscrn() {}
+	inline void clrscrn() {}
 
-	void consoleBlackOnWhite() {}
+	inline void consoleBlackOnWhite() {}
 
-	void consoleWhiteOnBlack() {}	
+	inline void consoleWhiteOnBlack() {}
 	
 	//takes int x and y for coordinates to move cursor to.
-	void cursor(int x, int y) {}
+	inline void cursor(int x, int y) {}
 #endif
 
 
